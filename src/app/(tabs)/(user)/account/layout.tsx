3404ecc7e91a6efe1c3450a6a8c634b98ceba315
alikashanidev <alikashani092@gmail.com>
2024-09-30 19:07:58 +0330

    Add AccountProvider component

--- src/app/(tabs)/(user)/account/layout.tsx
+++ src/app/(tabs)/(user)/account/layout.tsx
@@ -1,17 +1,20 @@
+import { getSession } from "@/authentication";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 import CompleteRegister from "./_components/complete-register/CompleteRegister";
 import Sidebar from "./_components/sidebar/Sidebar";
-import { getSession } from "@/authentication";
-import { redirect } from "next/navigation";
+import AccountProvider from "./AccountProvider";
 
 export default async function DashboardLayout({ children }: PropsWithChildren) {
     const session = await getSession();
     if (!session) redirect("/signin");
     return (
-        <div className="flex lg:gap-2 lg:pt-14 lg:p-2 overflow-x-hidden">
-            <Sidebar />
-            <div className="w-full">{children}</div>
-            <CompleteRegister />
-        </div>
+        <AccountProvider>
+            <div className="flex lg:gap-2 lg:pt-14 lg:p-2 overflow-x-hidden">
+                <Sidebar />
+                <div className="w-full">{children}</div>
+                <CompleteRegister />
+            </div>
+        </AccountProvider>
     );
 }
